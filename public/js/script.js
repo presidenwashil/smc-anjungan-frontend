a987af14cc35c0acd390cb38f2a023764f467e96
presidenwashil <raihanpresiden2003@gmail.com>
2023-11-30 15:37:53 +0800

    Add Antrian Poli
    penambahan halaman antrian poli dan beberapa perbaikan pada script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,21 +1,13 @@
-<script>
-      function startScrollAnimation() {
-          const scrollingContent = document.getElementById('scrollingContent');
-          scrollingContent.style.animation = 'none';
-          void scrollingContent.offsetWidth;
-          scrollingContent.style.animation = null; 
-          scrollingContent.style.animationPlayState = 'running';
-          setTimeout(() => {
-              scrollingContent.classList.add('hidden');
-          }, 15000);
-      };
-      document.addEventListener('DOMContentLoaded', function () {
-          startScrollAnimation();
-      });
-      setInterval(function () {
-          updateDateTime();
-          const scrollingContent = document.getElementById('scrollingContent');
-          scrollingContent.classList.remove('hidden');
-          startScrollAnimation();
-      }, 60000);
-</script>
+function refreshPage() {
+    setTimeout(function () {
+        location.reload(true); // Reload halaman dengan membersihkan cache
+    }, 40000); // Waktu dalam milidetik (60 detik)
+    }
+    document.addEventListener('DOMContentLoaded', function () {
+refreshPage();
+});
+
+setInterval(function () {
+    updateDateTime();
+    refreshPage();
+}, 40000);
